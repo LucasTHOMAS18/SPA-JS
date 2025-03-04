af8a69347203abd606579752b4346a9c58253447
MaitreRenard18 <maitre.renardowo@gmail.com>
2025-03-04 14:19:38 +0100

    Style

--- views/listingView.js
+++ views/listingView.js
@@ -4,6 +4,9 @@ export async function loadListing() {
     const vaisseaux = await getVaisseaux();
     const app = document.getElementById('app');
     app.innerHTML = '<h1>Liste des Vaisseaux</h1>' + vaisseaux.map(p =>
-        `<div onclick="router('detail', ${p.id})">${p.nom}</div>`
+        `<div class="horizontal-card" onclick="router('detail', ${p.id})">`
+        + `<img src="${p.image}" alt="${p.nom}">`
+        + `<h2>${p.nom}</h2>`
+        + `</div>`
     ).join('');
 }
